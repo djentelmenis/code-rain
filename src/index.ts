b5d79aec637b8614ba2c168f576b9810f355ced3
Kaspars Putniņš <kaspars.putnins19@gmail.com>
2021-01-04 20:47:12 +0200

    Added char shadows and color transition

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import { v4 as uuid } from "uuid";
 
 const CHARSET =
   'ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｦｲｸｺｿﾁﾄﾉﾌﾔﾖﾙﾚﾛﾝ012345789":・.=*+-<>¦｜&çﾘｸ日';
-const MAX_CHAR_SIZE = 35;
-const MIN_LINE_HEIGHT = 30;
-const MAX_LINE_HEIGHT = 110;
+const CHAR_MAX_SIZE = 30;
+const CHAR_SHADOW_BLUR = 8;
+const CHAR_FLIP_RATE = 0.0006;
+
+const LINE_MIN_HEIGHT = 30;
+const LINE_MAX_HEIGHT = 110;
+
 const LINE_SPAWN_RATE = 1.2;
-const FRAMES_PER_CHAR = 5;
+
+const FRAMES_PER_CHAR = 4;
 const LINE_DRAWN_FRAMES = 60;
 const LINE_FADE_FRAMES = 120;
 
@@ -42,15 +47,20 @@ class Char {
   private char: string;
 
   constructor() {
-    this.char = CHARSET.charAt(Math.floor(Math.random() * CHARSET.length - 1));
+    this.char = this.getRandomChar();
   }
 
+  private getRandomChar = () =>
+    CHARSET.charAt(Math.floor(Math.random() * CHARSET.length - 1));
+
   public getChar = () => this.char;
+  public flipChar = () => (this.char = this.getRandomChar());
 }
 
 class Line {
   private x: number;
   private y: number;
+  private screenHeight: number;
   private size: number;
   private chars: Char[] = [];
   private startingChar = 0;
@@ -59,13 +69,14 @@ class Line {
   private status: LineStates = LineStates.DRAWING;
   private charColor = "rgba(0, 128, 0, 1)";
 
-  constructor(x: number, y: number) {
+  constructor(x: number, y: number, screenHeight: number) {
     this.x = x;
     this.y = y;
-    this.size = Math.floor(Math.random() * MAX_CHAR_SIZE);
+    this.screenHeight = screenHeight;
+    this.size = Math.floor(Math.random() * CHAR_MAX_SIZE);
     for (
       let i = 0;
-      i < Math.floor(MIN_LINE_HEIGHT + Math.random() * MAX_LINE_HEIGHT);
+      i < Math.floor(LINE_MIN_HEIGHT + Math.random() * LINE_MAX_HEIGHT);
       i++
     ) {
       this.chars.push(new Char());
@@ -76,10 +87,39 @@ class Line {
     brush.save();
     brush.font = `${this.size}px Arial`;
     for (let i = this.startingChar; i < this.charsToDraw; i++) {
-      brush.fillStyle =
-        this.status === LineStates.DRAWING && i === this.charsToDraw - 1
-          ? "white"
-          : this.charColor;
+      // Flips a random amount of chars
+      if (Math.random() < CHAR_FLIP_RATE) this.chars[i].flipChar();
+
+      // Selects a color for the char
+      if (this.status !== LineStates.DRAWING) brush.fillStyle = this.charColor;
+      else {
+        switch (i) {
+          case this.charsToDraw - 4:
+            brush.fillStyle = "#24a524";
+            break;
+
+          case this.charsToDraw - 3:
+            brush.fillStyle = "#65c165";
+            break;
+
+          case this.charsToDraw - 2:
+            brush.fillStyle = "#b3e0b3";
+            break;
+
+          case this.charsToDraw - 1:
+            brush.fillStyle = "#fff";
+            break;
+
+          default:
+            brush.fillStyle = this.charColor;
+            break;
+        }
+      }
+
+      // Add shadows to chars
+      brush.shadowBlur = CHAR_SHADOW_BLUR;
+      brush.shadowColor = this.charColor;
+
       brush.fillText(this.chars[i].getChar(), this.x, this.y + this.size * i);
     }
     brush.restore();
@@ -93,7 +133,10 @@ class Line {
         if (this.frame > FRAMES_PER_CHAR) {
           this.charsToDraw++;
           this.frame = 0;
-          if (this.charsToDraw === this.chars.length) {
+          if (
+            this.charsToDraw === this.chars.length ||
+            this.charsToDraw * this.size > this.screenHeight
+          ) {
             this.status = LineStates.DRAWN;
           }
         }
@@ -145,7 +188,7 @@ const main = () => {
     requestAnimationFrame(animate);
 
     if (Math.random() * LINE_SPAWN_RATE > 1) {
-      lines.set(uuid(), new Line(Math.floor(Math.random() * width), 0));
+      lines.set(uuid(), new Line(Math.floor(Math.random() * width), 0, height));
     }
 
     // Draw black background
